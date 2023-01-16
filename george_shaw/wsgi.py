8a31b4c036a75da9717a30b0f939a83fa45c84e8
Jonnywdev <jonnywhitt19@gmail.com>
2023-01-16 14:06:33 +0000

    initial commit

---
+++ george_shaw/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for george_shaw project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'george_shaw.settings')
+
+application = get_wsgi_application()
