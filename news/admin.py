2f1f3c9d38f5d4f13be2a7a781e3f0a3237c1aaa
Jonnywdev <jonnywhitt19@gmail.com>
2023-02-22 17:58:36 +0000

    Add new items to news - models

--- news/admin.py
+++ news/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import RecentNews
 # Register your models here.
 
+
 admin.site.register(RecentNews)
--- news/models.py
+++ news/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-from profiles.models import UserProfile
+
 # Create your models here.
 
 
 class RecentNews(models.Model):
-    title = models.CharField(max_length=225)
-    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
     body = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return self.title
