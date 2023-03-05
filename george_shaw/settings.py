9f0760bf950f1f75874566c21ac9cf053e4377de
Jonnywdev <jonnywhitt19@gmail.com>
2023-03-05 23:28:56 +0000

    debug

--- george_shaw/settings.py
+++ george_shaw/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['george-shaw-brewery.herokuapp.com', 'localhost']
 
@@ -180,6 +180,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000'
     }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'george-shaw-brewery'
     AWS_S3_REGION_NAME = 'eu-west-2'
